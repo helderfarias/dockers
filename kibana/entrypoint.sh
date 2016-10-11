39be2d78708b49aa99350098c698ad980988011e
Helder Farias <helderfarias@gmail.com>
2016-10-11 14:43:23 -0300

    construcao

--- kibana/entrypoint.sh
+++ kibana/entrypoint.sh
@@ -5,4 +5,4 @@ if [ "$ELASTICSEARCH_URL" ]; then
     sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /opt/kibana/config/kibana.yml
 fi
 
-exec "/usr/bin/supervisord-c /etc/supervisor/conf.d/supervisor.conf"
+/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
